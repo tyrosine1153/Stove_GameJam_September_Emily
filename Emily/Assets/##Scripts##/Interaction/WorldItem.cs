2176c8121dc0942c5054e42d5b4489b49b6bdeb5
cj_park <jineyne@gmail.com>
2021-09-25 06:00:11 +0900

    feat: Add WorldItem to interact item

---
+++ Emily/Assets/##Scripts##/Interaction/WorldItem.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WorldItem : Interactable
+{
+    public enum WorldItemInteractType
+    {
+        Story,
+        PickUp
+    };
+
+    public WorldItemInteractType interactType;
+    public string itemId;
+    public StoryScriptableObject story;
+
+    public override void Interact()
+    {
+        switch (interactType)
+        {
+            case WorldItemInteractType.Story:
+                if (story) {
+                    UIManager.instance.ShowStory(story);
+                }
+                break;
+
+            case WorldItemInteractType.PickUp:
+            {
+                var item = ItemManager.instance.GetItemById(itemId);
+                if (item != null)
+                {
+                    InventoryManager.instance.AddItem(item);
+                }
+            }
+                break;
+
+            default:
+                break;
+        }
+    }
+}
