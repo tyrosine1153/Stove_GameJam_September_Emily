9165f87417242fd991f7319a10ec7a535a35fcaf
cj_park <jineyne@gmail.com>
2021-09-25 04:20:41 +0900

    feat: Add Item

---
+++ Emily/Assets/##Scripts##/Inventory/Item.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using UnityEngine;
+
+[Serializable]
+public class Item {
+    public string Id;
+    public string Name;
+    public Sprite Sprite;
+    public StoryScriptableObject Story;
+};
