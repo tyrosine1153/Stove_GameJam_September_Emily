ab04bd295f6d13c5f55cb32c74a84f578a376122
Ws-Peroth <a01033056809@gmail.com>
2021-09-25 00:36:58 +0900

    Edit Singleton
    Singleton에 주석 추가

--- Emily/Assets/Sington/Singleton.cs
+++ Emily/Assets/Sington/Singleton.cs
@@ -1,8 +1,20 @@
-using UnityEngine;
+﻿using UnityEngine;
+
+// 예시에서는 접근 제한 지시자 (public, private, protected)는 public으로 통일하였습니다.
+
+// 싱글톤을 사용 할 클래스에 상속을 받아 사용한다. 
+// ex : public class ClassName : Singleton<ClassName>  { }
+
+// 외부에서 접근할 경우 className.instance.참조할_변수/함수
+// ex : TestClass.instance.print("Hello, World");
+
+// OnAwake 함수는 override 하여 사용한다.
+// ex : public override void OnAwake()
 
 [DisallowMultipleComponent]
 public class Singleton<T> : MonoBehaviour where T : Singleton<T>
 {
+    // 상속 받은 객체에서 해당 bool 변수를 true / false 해 주는 것으로 싱글톤을 상속한 GameObject의 dontDestroyOnLoad를 on / off 가능함
     public bool dontDestroyOnLoad;
 
     private static volatile T _instance;
