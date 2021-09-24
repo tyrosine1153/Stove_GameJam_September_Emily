4d8914e2f202ead92d254f2437892ef18e28868d
cj_park <jineyne@gmail.com>
2021-09-25 05:18:21 +0900

    demo: Add demo sources

---
+++ Emily/Assets/##Scripts##/Inventory/WorldItem.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class WorldItem : MonoBehaviour
+{
+    public Button btn1;
+
+    void Awake()
+    {
+        btn1.onClick.AddListener(btn1Click);
+    }
+
+    private void btn1Click()
+    {
+        var item = ItemManager.instance.GetItemById("evidence_1");
+
+        if (item != null)
+        {
+            InventoryManager.instance.AddItem(item);
+        }
+    }
+}
