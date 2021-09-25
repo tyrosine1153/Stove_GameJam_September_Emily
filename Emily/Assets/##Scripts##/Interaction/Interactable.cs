a9dcb1e845ed80eb249af62666bb53cef190841b
cj_park <jineyne@gmail.com>
2021-09-25 16:01:35 +0900

    feat: Update Interactable to drag

--- Emily/Assets/##Scripts##/Interaction/Interactable.cs
+++ Emily/Assets/##Scripts##/Interaction/Interactable.cs
@@ -1,10 +1,22 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 [RequireComponent(typeof(BoxCollider2D))]
-public class Interactable : MonoBehaviour
-{
+[RequireComponent(typeof(Rigidbody2D))]
+public class Interactable : MonoBehaviour {
+    public bool Draggable { get { return draggable; } }
+
+    [SerializeField]
+    protected bool draggable = false;
+
+    public void Start()
+    {
+        var rg = GetComponent<Rigidbody2D>();
+        rg.bodyType = RigidbodyType2D.Kinematic;
+    }
+
     public virtual void Interact()
     {
         // do nothing
