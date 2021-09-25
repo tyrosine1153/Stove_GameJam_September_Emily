08b788b408445f063adfaef0c5e56d16aaa297f5
oeccsy <oeccsy@gmail.com>
2021-09-25 17:14:16 +0900

    Update PlaceChange.cs

--- Emily/Assets/##Scripts##/PlaceChange/PlaceChange.cs
+++ Emily/Assets/##Scripts##/PlaceChange/PlaceChange.cs
@@ -7,35 +7,38 @@ public class PlaceChange : MonoBehaviour
     //[SerializeField] GameObject placePool;
     [SerializeField] GameObject[] placeArr;
 
-    SpriteRenderer curRenderer;
-    SpriteRenderer nextRenderer;
-    int curIndex=0;
+    SpriteRenderer[] curRenderer;
+    SpriteRenderer[] nextRenderer;
+    int curIndex = 0;
     bool isChanging = false;
+    int prevIndex = 0;
     void Start()
     {
-        curRenderer = placeArr[curIndex].GetComponent<SpriteRenderer>();
+        curRenderer = placeArr[curIndex].GetComponentsInChildren<SpriteRenderer>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(isChanging)
+        if (isChanging)
         {
             DeleteCurPlace();
         }
         else
         {
-            if(Input.GetKeyDown(KeyCode.LeftArrow))
+            if (Input.GetKeyDown(KeyCode.LeftArrow))
             {
+                prevIndex = curIndex;
                 curIndex = (curIndex + placeArr.Length - 1) % placeArr.Length; // curIndex 인덱스 범위 초과시 마지막 Index로
                 CreateNextPlace();
             }
 
-            if(Input.GetKeyDown(KeyCode.RightArrow))
+            if (Input.GetKeyDown(KeyCode.RightArrow))
             {
-                curIndex = (curIndex + 1 ) % placeArr.Length; //curIndex 인덱스 범위 초과시 0으로
+                prevIndex = curIndex;
+                curIndex = (curIndex + 1) % placeArr.Length; //curIndex 인덱스 범위 초과시 0으로
                 CreateNextPlace();
-                
+
             }
         }
     }
@@ -43,23 +46,31 @@ public class PlaceChange : MonoBehaviour
     void CreateNextPlace()
     {
         placeArr[curIndex].SetActive(true);
-        nextRenderer = placeArr[curIndex].GetComponent<SpriteRenderer>();
-        nextRenderer.color = new Color(1f,1f,1f,0f);
+        nextRenderer = placeArr[curIndex].GetComponentsInChildren<SpriteRenderer>();
+        foreach (SpriteRenderer nextRen in nextRenderer)
+        {
+            nextRen.color = new Color(1f, 1f, 1f, 0f);
+        }
+
         isChanging = true;
     }
 
     void DeleteCurPlace()
     {
-        curRenderer.color -= new Color(0,0,0, 0.01f);
-        nextRenderer.color += new Color(0,0,0, 0.01f);
-
-        if(curRenderer.color.a < 0.01f && nextRenderer.color.a > 0.99f)
+        foreach (SpriteRenderer curRen in curRenderer)
         {
-            curRenderer.gameObject.SetActive(false);
+            curRen.color -= new Color(0, 0, 0, 0.01f);
+        }
+        foreach (SpriteRenderer nextRen in nextRenderer)
+        {
+            nextRen.color += new Color(0, 0, 0, 0.01f);
+        }
+
+        if (curRenderer[0].color.a < 0.01f && nextRenderer[0].color.a > 0.99f)
+        {
+            placeArr[prevIndex].SetActive(false);
             curRenderer = nextRenderer;
             isChanging = false;
         }
     }
 }
-
-
