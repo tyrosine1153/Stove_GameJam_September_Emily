71c7b011df3b9d41189f552775b98bd35313b517
oeccsy <oeccsy@gmail.com>
2021-09-26 04:25:04 +0900

    FinalChoice Scene Update

--- Emily/Assets/##Scripts##/SelectSuspect/SelectSuspect.cs
+++ Emily/Assets/##Scripts##/SelectSuspect/SelectSuspect.cs
@@ -5,35 +5,36 @@ using UnityEngine;
 public class SelectSuspect : MonoBehaviour
 {
     [SerializeField] Camera cam;
-    [SerializeField] GameObject confirmPanel;
+    //[SerializeField] GameObject confirmPanel;
     Vector2 mousePos;
     RaycastHit2D hit;
     Suspect curSuspect;
+    Suspect prevSuspect;
+    bool isSelect = false;
 
     void Update()
     {
         mousePos = cam.ScreenToWorldPoint(Input.mousePosition);
         hit = Physics2D.Raycast(mousePos, transform.forward);
         
-        if(hit)
+        if(hit && !isSelect)
         {
             curSuspect = hit.collider.GetComponent<Suspect>(); //콜라이더 넣는건 별로인가욥?
-            curSuspect.isMouseOn = true;
+        
+            StartCoroutine(SetMouseOn(curSuspect));
 
             if(Input.GetMouseButtonDown(0))
             {
+                isSelect = true;
                 curSuspect.isClick = true;
             }
-        }
-        else if(curSuspect != null)
-        {
-            curSuspect.isMouseOn = false;
         }
         
     }
 
-    void confirmMessage()
-    {
-        
+    IEnumerator SetMouseOn(Suspect curSuspect)
+    {   curSuspect.isMouseOn = true;
+        yield return new WaitForSeconds(0.2f);
+        curSuspect.isMouseOn = false;
     }
 }
